4afa478be88198ae10d75a3fa60a151a8fceeaf1
Uthaeus <romanlavery@gmail.com>
2023-02-08 16:16:13 -0700

    added a catch block on unseccessful http requests and added error styling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -9,10 +9,16 @@ import classes from './AvailableMeals.module.css';
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch('https://food-order-app-798cb-default-rtdb.firebaseio.com/meals.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!');
+      }
+
       const responseData = await response.json();
 
       const loadedMeals = [];
@@ -30,7 +36,11 @@ const AvailableMeals = () => {
       setIsLoading(false);
     }
 
-    fetchMeals();
+    fetchMeals().catch(error => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+    
   }, []);
 
     const mealsList = meals.map(meal => (
@@ -47,6 +57,14 @@ const AvailableMeals = () => {
       return (
         <section className={classes.mealsLoading}>
           <p>Loading...</p>
+        </section>
+      );
+    }
+
+    if (httpError) {
+      return (
+        <section className={classes.mealsError}>
+          <p>{httpError}</p>
         </section>
       );
     }
