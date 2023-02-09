dda7b4cae154af4e5ab85b04c3df3429a9df78d5
Uthaeus <romanlavery@gmail.com>
2023-02-08 19:41:12 -0700

    refactored code to conditionally show based on server response and added functions to cart-context for clearing input

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,9 +6,12 @@ import CartContext from '../../store/cart-context';
 import Checkout from './Checkout';
 
 import classes from './Cart.module.css';
+import React from 'react';
 
 const Cart = (props) => {
     const [isCheckout, setIsCheckout] = useState(false);
+    const [isSubmitting, setIsSubmitting] = useState(false);
+    const [didSubmit, setDidSubmit] = useState(false);
     const cartCtx = useContext(CartContext);
 
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -26,14 +29,18 @@ const Cart = (props) => {
         setIsCheckout(true);
     };
 
-    const submitOrderHandler = userData => {
-        fetch('https://food-order-app-798cb-default-rtdb.firebaseio.com/orders.json', {
+    const submitOrderHandler = async userData => {
+        setIsSubmitting(true);
+        await fetch('https://food-order-app-798cb-default-rtdb.firebaseio.com/orders.json', {
             method: 'POST',
             body: JSON.stringify({
                 user: userData,
                 orderedItems: cartCtx.items
             })
         });
+        setIsSubmitting(false);
+        setDidSubmit(true);
+        cartCtx.clearCart();
     }
 
     const cartItems = (
@@ -58,8 +65,8 @@ const Cart = (props) => {
         </div>
     );
 
-    return (
-        <Modal onClose={props.onClose}>
+    const cartModalContent = (
+        <React.Fragment>
             {cartItems}
             <div className={classes.total}>
                 <span>Total Amount</span>
@@ -67,6 +74,25 @@ const Cart = (props) => {
             </div>
             {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />}
             {!isCheckout && modalActions}
+        </React.Fragment>
+    );
+
+    const isSubmittingModalContent = <p>Sending order data....</p>;
+
+    const didSubmitModalContent = (
+        <React.Fragment>
+            <p>Successfully sent the order!</p>
+            <div className={classes.actions}>
+                <button className={classes.button} onClick={props.onClose}>Close</button>
+            </div>
+        </React.Fragment>
+    );
+
+    return (
+        <Modal onClose={props.onClose}>
+            {!isSubmitting && !didSubmit && cartModalContent}
+            {isSubmitting && isSubmittingModalContent}
+            {!isSubmitting && didSubmit && didSubmitModalContent}
         </Modal>
     );
 };
